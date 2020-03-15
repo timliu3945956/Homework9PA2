270ec6559f5c51c46375a10da7ef7468dc3bd02c
Tim Liu <timliu3945956@gmail.com>
2020-03-15 15:17:31 -0500

    My Project2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Programming Assignment #2
+//
+//  Created by Tim Liu on 2/25/20.
+//  Copyright © 2020 Tim Liu. All rights reserved.
+//
+
+#include <iostream>
+
+void displayVersion(double version);
+
+using namespace std;
+
+static double version = 1.2;
+
+// hello this is tim
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    displayVersion(version);
+    return 0;
+}
---
+++ version.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+void displayVersion(double version)
+{
+	cout << "Current version is " << version << endl;
+}
+
